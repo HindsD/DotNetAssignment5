d58a7e1ea1a7f369492c58139710191bffe1ff47
Danny Hinds <dhinds1@my.wctc.edu>
2021-10-02 21:56:11 -0500

    finishing touches- added nlog, an exception handler, and comments to navigate if i need to come back and change something

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using NLog.Web;
 
 namespace DotNetTicketSysCLASSES
 {
@@ -7,15 +8,20 @@ namespace DotNetTicketSysCLASSES
     {
         static void Main(string[] args)
         {
-            string file = "Tickets.csv";
-            Ticket ticket = new Ticket();
+            string path = Directory.GetCurrentDirectory() + "\\nlog.config";
+            var logger = NLogBuilder.ConfigureNLog(path).GetCurrentClassLogger(); // addslogger
+            logger.Info("Program started"); //logs it started
+
+            string file = "Tickets.csv"; //imports the csv where we will collect the tickets
+            Ticket ticket = new Ticket(); 
             string choice;
             do
             {
-                Console.WriteLine("1) List all tickets.");
+                Console.WriteLine("1) List all tickets."); //asks the user what they'd like to do
                 Console.WriteLine("2) Create new ticket.");
                 Console.WriteLine("Enter any other key to exit.");
                 choice = Console.ReadLine();
+                logger.Info("User choice: " + choice); //leeps track of the user's choice
 
                 if (choice == "1")
                 {
@@ -27,19 +33,19 @@ namespace DotNetTicketSysCLASSES
                         {
                             string line = sr.ReadLine();
                             ticket.arr = line.Split('|');
-                            Console.WriteLine(ticket.Display());
+                            Console.WriteLine(ticket.Display()); //reads the csv and displays each ticket
                             
                         }
                         sr.Close();
                     }
                     else
                     {
-                        Console.WriteLine("File does not exist");
+                        logger.Error("File does not exist: Tickets.csv"); // logs if it cant reach the csv
                     }
                 }
                 else if (choice == "2")
                 {
-                    StreamWriter sw = new StreamWriter(file);
+                    StreamWriter sw = new StreamWriter(file); //writes a new ticket
                     for (int i = 1; i < 10; i++)
                     {
                         ticket.ticketId = i;
@@ -58,12 +64,19 @@ namespace DotNetTicketSysCLASSES
                         ticket.assigned = Console.ReadLine();
                         Console.WriteLine("Who is watching?");
                         ticket.watching = Console.ReadLine();
-
-                        sw.WriteLine(ticket.WriteTicket());
+                            try{
+                                sw.WriteLine(ticket.WriteTicket()); //a  try catch that will throw an exception if it can't write a ticket
+                            }catch (Exception ex)
+                        {
+                            logger.Error("Unable to add ticket!"); // logs an exception
+                            logger.Error(ex.Message);
+                        }
                     }
                     sw.Close();
                 }
-            } while (choice == "1" || choice == "2");
+            } while (choice == "1" || choice == "2"); // keeps looping as long as 1 or 2 is entered
+            logger.Info("Program ended"); // logs that it ended
         }
+        
     }
 }
